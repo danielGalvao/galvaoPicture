c23f61910f2454cfd9253f7055eae3146fb93004
Daniel Galvão <galvaodev@gmail.com>
2016-11-17 23:44:57 -0200

    Using parameters to show different pictures

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './foto.component.html'
 })
 
-export class FotoComponent {}
+export class FotoComponent {
+
+  @Input() title;
+  @Input() url;
+}
