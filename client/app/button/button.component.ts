b4ef5662e6b0e70b41bc44c85ee10e9fd02376dc
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-12-13 08:56:46 -0200

    Fix output and data bind btn

--- client/app/button/button.component.ts
+++ client/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -12,7 +12,7 @@ export class ButtonComponent {
   @Input() estilo: string = 'btn-default';
   @Input() tipo: string = 'button';
   @Input() desable: boolean = false;
-  action = new EventEmitter();
+  @Output() action = new EventEmitter();
 
   execAction() {
      if(confirm('Deseja realmente excluir?')) {
