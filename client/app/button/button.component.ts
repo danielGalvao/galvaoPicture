d02ce10c3cfa52c1d69a4131b2deda16126e2ee7
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-12-13 08:54:24 -0200

    Add eventemitter

--- client/app/button/button.component.ts
+++ client/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -12,5 +12,11 @@ export class ButtonComponent {
   @Input() estilo: string = 'btn-default';
   @Input() tipo: string = 'button';
   @Input() desable: boolean = false;
+  action = new EventEmitter();
 
+  execAction() {
+     if(confirm('Deseja realmente excluir?')) {
+      this.action.emit(null);
+     }
+  }
 }
